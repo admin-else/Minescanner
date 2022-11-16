82c5619a8eb59869672f344b35c148b4390b5e4a
Admin Else <74665496+admin-else@users.noreply.github.com>
2022-11-16 17:45:04 +0100

    SUPER FAST WARNING

--- Scraper.py
+++ Scraper.py
@@ -8,11 +8,18 @@ from multiprocessing.pool import ThreadPool
 import concurrent.futures
 import pinger
 
-def masscan(ip):
-    if ip==None:
-        return
-    ipparts = ip.split('.')
-    os.system(f"sudo masscan -p 25565 --wait=0 --rate 10000 -oX output.xml --range {ipparts[0]}.{ipparts[1]}.{ipparts[2]}.0-{ipparts[0]}.{ipparts[1]}.{ipparts[2]}.255")
+def serverlistping(ip):
+    try:
+            pinger.main(ip)
+    except:
+        pass
+
+def masscan(ipranges):
+    lines = []
+    for iprange in ipranges:
+        lines.append('range = '+ iprange+'.0-'+iprange+'.255\n')
+        open('./masscan.conf','w').writelines(lines)
+    os.system(f"sudo masscan -c ./masscan.conf -p 25565 --wait=0 --rate 10000 -oX output.xml")
     soup = BeautifulSoup(open("output.xml",'r').read(),'lxml')
     hosts = soup.find_all('address')
     for host in hosts:
@@ -44,44 +51,23 @@ if __name__=='__main__':
     addrslist = []
     iprages = []
     iplist = []
-    #lastpage = int(str(BeautifulSoup(requests.get('https://minecraft-server-list.com/').content,'lxml').find('a', string='>>').get('href')).split('/')[2])
-    lastpage = 3
+    lastpage = int(str(BeautifulSoup(requests.get('https://minecraft-server-list.com/').content,'lxml').find('a', string='>>').get('href')).split('/')[2])
+    #lastpage = 3
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for ips in executor.map(scaper2, range(1, lastpage)):
             if ips!=None:
                 addrslist=addrslist+ips
     addrslist = list(dict.fromkeys(addrslist))
     print('done downloading ips')
-    with concurrent.futures.ProcessPoolExecutor(max_workers=70) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=200) as executor:
         for range in executor.map(tcpping, addrslist):
             if range!=None and range not in iprages:
                 iprages.append(range)
     print('done pinging ips')
-    i=1
+    masscan(iprages)
     for ip in iprages:
-        masscan(ip)
-        os.system("clear")
-        print(f"{round(i/len(iprages)*100,2)}% Done with masscaning!")
-        i+=1
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(pinger.main,iplist)
-    
-        
-
-
-    exit()
-    tcpping()
-    i = 1
-    for ip in dupelicateaddress:
-        masscan(ip)
-        os.system("clear")
-        print(f"{round(i/len(dupelicateaddress)*100,2)}% Done with masscaning!")
-        i+=1
-    for ip in masscanedips:
         try:
-            subprocess.run(['python3', './pinger.py', ip], timeout=2)
+            subprocess.run(['python3','/home/simon/src/python/minescaner/pinger.py',ip], timeout=2)
         except:
             pass
-        print(f"{round(i/len(masscanedips)*100,2)}% Done with pinging!")
-        i+=1
-    f.close()
+        print('pinged')
